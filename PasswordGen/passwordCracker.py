1aeffe2629a87a862039fd01c01b9ee9e62c68bc
blakeshucart <78447214+blakeshucart@users.noreply.github.com>
2021-03-31 23:50:29 -0500

    Now doesnt know password length, more realistic

--- PasswordGen/passwordCracker.py
+++ PasswordGen/passwordCracker.py
@@ -9,13 +9,22 @@ userPwd = input("Enter Password to check: ")
 
 guessPwd = ""
 startTime = time.time()
-
+count = 0
+pwdLength = 1
 while(guessPwd != userPwd):
-    guessPwd = random.choices(charsList, k=len(userPwd))
+    guessPwd = random.choices(charsList, k=pwdLength)
 
     print("<============" + str(guessPwd) + "============>")
+    count+=1
+    if(count > 10000):
+        count = 0
+        pwdLength+=1
+    
+    if(pwdLength > 10):
+        pwdLength = 1
 
     if(guessPwd == list(userPwd)):
         print("Your password is: " + "".join(guessPwd))
         print("It took %s seconds to crack this password" % (time.time() - startTime))
+        print("It took %s minutes to crack this password" % ((time.time() - startTime)/60))
         break
