96b3f73265df63128ff1007a6ed203a0323b3b6b
averywald <averywald.dev@gmail.com>
2021-04-02 17:36:48 -0500

    made a lil SHA-256 hash script

---
+++ password/cracker/hasher.py
@@ -0,0 +1,12 @@
+import hashlib
+import sys # TODO: get argparse to work instead of sys?
+
+# can be called from another script
+def hasher(input):
+    i = input.encode(encoding='UTF-8') # encode the input for hashing
+    return hashlib.sha256(i).hexdigest() # hash it
+
+# if the script is called from CLI w/ hash input arg
+if (len(sys.argv) > 1): # we got a password to hash
+    hashed = hasher(sys.argv[1]) # get function output
+    print(f'input: {sys.argv[1]}\noutput: {hashed}') # print output
