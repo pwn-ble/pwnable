19b4ea9ad206bbafd42c00b5ffcf39512757e1f7
avery <remy@kali.remy.net>
2021-04-09 03:01:09 -0500

    almost done for demo

--- src/modules/password/ui.py
+++ src/modules/password/ui.py
@@ -1,6 +1,6 @@
-from guizero import App, Box, PushButton, Slider, Text, TextBox
+from guizero import App, Box, info, PushButton, Slider, Text, TextBox, Window
 import subprocess
-from generator import generator
+# from generator import generator
 from hasher import hasher
 
 def check_password():
@@ -15,16 +15,35 @@ def check_password():
     strengthSlider.value = len(inputBox.value) # alter the value
     strengthSlider.enabled = False # disable it again to prevent manual change
 
-def generate_password(length=15):
-    pwd = generator.gen(length) # call password synthesis function
-    suggestionOutputLabel.visible = True # show suggestion output label
-    suggestionOutputLabel.value = f'Suggested password: {pwd}' # output to the UI
+# def generate_password(length=15):
+#     pwd = generator.gen(length) # call password synthesis function
+#     suggestionOutputLabel.visible = True # show suggestion output label
+#     suggestionOutputLabel.value = f'Suggested password: {pwd}' # output to the UI
 
 def submit():
     passwd = hasher.hash(inputBox.value) # hash the input from the textbox
     hasher.unshadow(passwd) # write hash to unshadowed john format
 
-    subprocess.call(['python3', 'modules/password/cracker/ui.py']) # TODO: os.chdir() ? app.py that calls ui.py ?
+    title = 'an intro to passwords'
+    b = f'your password has been encrypted.\nthis is what it looks like in the OS: {passwd}'
+    i = info(title, b)
+    b = "this is done so that your password isn't stored in plaintext for everyone to see"
+    i = info(title, b)
+    # another prompt
+    b = ""
+    i = info(title, b)
+    # open up a terminal
+    
+    # popup with john command
+    b = ""
+    i = info(title, b)
+    # run in terminal, wait for output
+    # out = subprocess.Popen()
+    # prompt saying this is why its important
+    b = ""
+    i = info(title, b)
+
+    # more?
 
 #
 # GuiZero UI components
@@ -32,18 +51,26 @@ def submit():
 
 app = App(title="password strength") # create a window
 
-inputLabel = Text(app, text="enter a password to test strength")
+prompt = """
+    passwords are very important, they protect all of your data.
+    you should make sure to use strong passwords, 
+    because most of them can be hacked.
+    """
+
+promptInfoBlurb = info("an intro to passwords", prompt) # present an info blurb
+
+inputLabel = Text(app, text="enter a password to test its strength")
 
 inputContainer = Box(app, layout="grid")
 inputBox = TextBox(inputContainer, grid=[0,1]) # add a textbox for input
 inputBox.update_command(check_password) # check pass strength after any input
 inputErrorLabel = Text(inputContainer, text="password should be 6-8 characters", grid=[0,0], color="red", visible=False)
-sliderLabel = Text(app, text="password strength:")
+sliderLabel = Text(app, text="password length:")
 strengthSlider = Slider(app, enabled=False) # add slider to show pass strength
 
-suggestionContainer = Box(app, layout="grid")
-suggestionButton = PushButton(app, text="get a password suggestion", command=generate_password) # button to get a suggested password
-suggestionOutputLabel = Text(app, visible=False) # initially invisible
+# suggestionContainer = Box(app, layout="grid")
+# suggestionButton = PushButton(app, text="get a password suggestion", command=generate_password) # button to get a suggested password
+# suggestionOutputLabel = Text(app, visible=False) # initially invisible
 
 submitButton = PushButton(app, text="submit", enabled=False, command=submit)
 
