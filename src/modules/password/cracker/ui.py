2989c28532c6dab1caf422944aaf0716db0dc613
avery <remy@kali.remy.net>
2021-04-08 22:09:06 -0500

    added a little fake prompt

--- src/modules/password/cracker/ui.py
+++ src/modules/password/cracker/ui.py
@@ -6,4 +6,11 @@ from guizero import App, Box, PushButton, Slider, Text, TextBox
 
 app = App(title="password cracker")
 
+prompt = """
+this is a multiline string.
+i hope that it works in the UI
+"""
+
+promptLabel = Text(app, text=prompt)
+
 app.display()
