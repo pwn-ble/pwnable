55a8f0f1acebadefbeacf9176edddc8f6d95ef97
avery <averywald.dev@gmail.com>
2021-04-10 17:17:04 -0500

    moved shell script to the python file

--- src/modules/cli/tutorial.py
+++ src/modules/cli/tutorial.py
@@ -1,7 +1,26 @@
+import os
 import subprocess
-import tkinter as tk
 
-def open_terminal_win():
-    popen = subprocess.Popen(["xterm"], stdinstdout=subprocess.PIPE, shell=True)
+# should be ran after segue from tkinter UI stuff
 
-subprocess.call(['sh', 'tutorial.sh'])
+os.chdir('src/modules/cli/')
+
+print('welcome to the terminal!')
+
+print('what is your name?')
+name = input()
+
+print(f'nice to see you, {name}.\nwe are going to learn how to use a terminal')
+print('are you ready?')
+answer = input()
+
+if (answer == 'yes'):
+    print('great!')
+elif (answer == 'no'):
+    print('well too bad! ')
+else:
+    print('what? it was a yes or no question.')
+
+print('lets get started')
+
+subprocess.Popen(['/bin/zsh'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
