7edd5eb808887daf7bdab7538a96c0a0b121bec6
avery <remy@kali.remy.net>
2021-04-10 12:47:31 -0500

    tryna get terminal output piped into script

--- src/gui/terminalscreen.py
+++ src/gui/terminalscreen.py
@@ -2,23 +2,33 @@ import tkinter as tk
 from tkinter.messagebox import showwarning
 import subprocess
 
-root = tk.Tk()
-root.geometry("600x500")
+def open_terminal_win():
+    popen = subprocess.Popen(["xterm"], stdout=subprocess.PIPE)
+    
+    for stdout_line in iter(popen.stdout.readline, ""):
+        print(stdout_line)
+        
+    # popen.stdout.close()
+    return_code = popen.wait()
 
-label = tk.Label(root, text="Example of xterm embedded in frame")
-label.pack(fill=tk.X)
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
 
-xterm_frame = tk.Frame(root)
-xterm_frame.pack(fill=tk.BOTH, expand=True)
+open_terminal_win()
 
-xterm_frame_id = xterm_frame.winfo_id()
+# root = tk.Tk()
+# root.geometry("600x500")
 
-try:
-    p = subprocess.Popen(
-        ["xterm", "-into", str(xterm_frame_id), "-geometry", "80x20"],
-        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-except FileNotFoundError:
-    showwarning("Error", "xterm is not installed")
-    raise SystemExit
+# label = tk.Label(root, text="Example of xterm embedded in frame")
+# label.pack(fill=tk.X)
 
-root.mainloop()
+# xterm_frame = tk.Frame(root)
+# xterm_frame.pack(fill=tk.BOTH, expand=True)
+
+# xterm_frame_id = xterm_frame.winfo_id()
+
+# except FileNotFoundError:
+#     showwarning("Error", "xterm is not installed")
+#     raise SystemExit
+
+# root.mainloop()
