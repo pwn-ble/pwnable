ac1b94d81ddc5f9a59e746d6b51a805a2a6c8c1a
avery <averywald.dev@gmail.com>
2021-04-10 20:40:20 -0500

    using shell subprocess, tkinter help popup windows

--- src/modules/cli/tutorial.py
+++ src/modules/cli/tutorial.py
@@ -1,26 +1,14 @@
 import os
 import subprocess
+import tkinter
 
 # should be ran after segue from tkinter UI stuff
 
 os.chdir('src/modules/cli/')
 
-print('welcome to the terminal!')
+root = tkinter.Tk()
 
-print('what is your name?')
-name = input()
+# open a terminal window for the user
+terminal_win = subprocess.Popen(['x-terminal-emulator'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-print(f'nice to see you, {name}.\nwe are going to learn how to use a terminal')
-print('are you ready?')
-answer = input()
-
-if (answer == 'yes'):
-    print('great!')
-elif (answer == 'no'):
-    print('well too bad! ')
-else:
-    print('what? it was a yes or no question.')
-
-print('lets get started')
-
-subprocess.Popen(['/bin/zsh'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+root.mainloop()
