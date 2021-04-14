80cd3ec31288b86bc50a890a57ebefd511183844
averywald <averywald.dev@gmail.com>
2021-04-14 15:12:51 -0500

    login_service checks if user exists in a fake file, need to implement users command, need to implement useradd command

--- src/app.py
+++ src/app.py
@@ -9,25 +9,20 @@ i read something about __main__ and directory structure, but need to do some
 more research to get that working
 """
 
-def decode_stdout(input):
-    return input.decode('utf-8').rstrip('\r|\n')
-
 # lauch pwnable home screen script
 with subprocess.Popen(['python3', './src/gui/login.py'], stdout=subprocess.PIPE) as login:
     creds = {} # dictionary for credential key-value-pairs
 
     for line in login.stdout:
-        output = decode_stdout(line) #strip byte-encoding from stdout
+        output = login_service.decode_stdout(line) #strip byte-encoding from stdout
         cred_line = output.split() # separate credentials from their label in the stdout
         creds[cred_line[0].strip(':')] = cred_line[1] # add credentials to dictionary as kvp
     login.wait()
 
     # check for user in system
-    with subprocess.Popen(['cat', './src/service/fake_users.txt'], stdout=subprocess.PIPE) as checker:
-        output = decode_stdout(checker.stdout.read())
-        for user in output.split('\n'):
-            if (creds['user'] == user): # 
-                print('duplicate username')
-            else:
-                print('should create a new system user')
-                break
+    if (login_service.check_user(creds['user'])):
+        print('user already exists')
+        # should have them try a different usrname
+    else:
+        print('great, lets get you set up...')
+        # should call login_service.adduser(creds['user'])
--- src/service/login_service.py
+++ src/service/login_service.py
@@ -5,9 +5,23 @@ import os
 # def add_user(uname: str, passwd: str):
 #     with Popen(["users"], stdout=PIPE) as proc:
 #         log.write(proc.stdout.read())
-    
-def check_user(uname: str):
-    with Popen(["users"], stdout=PIPE) as proc:
-        output = proc.stdout.read()
-        print(output) # need to decode
-        return output
+
+def check_user(uname: str) -> bool:
+    """
+    uname: the proposed username to assign to a new user
+
+    returns a boolean indicating whether the proposed username is already taken or not
+    """
+    with subprocess.Popen(['cat', './src/service/fake_users.txt'], stdout=subprocess.PIPE) as checker:
+        output = decode_stdout(checker.stdout.read()) # get process output
+        
+        for user in output.split('\n'):
+            if (uname == user): # if user exists in system
+                return True # found, so user should try a different username
+            else:
+                print('should create a new system user')
+                # handle user creation
+        return False # didn't find a user with that name, should be good to make one on the system
+
+def decode_stdout(input) -> str:
+    return input.decode('utf-8').rstrip('\r|\n')
