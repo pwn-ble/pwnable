105e3146f6f0bd26f637a621b6db8b943ea1fa14
averywald <averywald.dev@gmail.com>
2021-04-15 17:56:08 -0500

    tkinter frame handles command as subprocess, DOES NOT PASS TO TERMINAL

--- src/app.py
+++ src/app.py
@@ -8,4 +8,27 @@ if (platform.system() == 'Windows'):
 else:
     subprocess.call(['python3', './gui/menu.py'])
 
-# should contain some generic loader functions ??
+# class Application(tk.Frame):
+#     def __init__(self):
+#         self.root = tk.Tk()
+#         self.root.geometry("300x200")
+
+#         tk.Frame.__init__(self, self.root)
+#         self.create_widgets()
+
+#     def create_widgets(self):
+#         self.root.bind('<Return>', self.parse)
+#         self.grid()
+
+#         self.submit = tk.Button(self, text="Submit")
+#         self.submit.bind('<Button-1>', self.parse)
+#         self.submit.grid()
+
+#     def parse(self, event):
+#         print("You clicked?")
+
+#     def start(self):
+#         self.root.mainloop()
+
+
+# Application().start()
--- src/modules/cli/tutorial.py
+++ src/modules/cli/tutorial.py
@@ -1,49 +1,102 @@
 import os
 import subprocess
+import shlex
 import tkinter as tk
 
+# TODO: move this to a UI component file
 class Tutorial(tk.Frame):
 
-    def __init__(self, root: tk.Tk):
+    def __init__(self, root:tk.Tk, title:str, prompt:str):
         tk.Frame.__init__(self, root) # call tk superclass constructor
-        self.master = root
 
-        root.wm_title("hwllo from tuorial class") # set window title
+        self.command_var = tk.StringVar() # set up input tracking variable
+
+        self.create_widgets(root, title, prompt) # add elements to the app root
+
+    # TODO: def clean_input(input: str):
+
+    def create_widgets(self, root, title, prompt):
+        # assign attributes
+        self.master = root
+        self.master.bind('<Return>', self.handle_input) # submit on return key press
+
+        self.window_title = title
+        self.prompt = prompt
+
+        root.wm_title(self.window_title) # set window title
         
         # add UI elements
-
-
-        # print('welcome to the terminal!')
-        # # kun here, you can also do 
-        # # name = input('what is your name?: ')
-        # print('what is your name?')
-        # name = input()
-
-        # print(f'nice to see you, {name}.\nwe are going to learn how to use a terminal')
-        # # here as well
-        # # answer = input('are you ready?: ')
-        # print('are you ready?')
-        # answer = input()
-
-        # if (answer == 'yes'):
-        #     print('great!')
-        # elif (answer == 'no'):
-        #     print('well too bad! ')
-        # else:
-        #     print('what? it was a yes or no question.')
-
-        # print('lets get started')
-
-        # open a terminal window for the user
+        self.prompt_label = tk.Label(self.master, text=self.prompt).pack()
+        self.command_input = tk.Entry(self.master, textvariable=self.command_var).pack()
+        self.submit_button = tk.Button(self.master, command=self.handle_input).pack()
         
-    
-    # def handle_input():
-        # do we need input?
+    def handle_input(self, event):
+        """
+        event: does NOT need to be assigned - tkinter passes this from command
+        """
+        cmd = shlex.split(self.command_var.get()) # TODO: clean input for quotes and stuff
+        self.command_var.set('')
 
+        self.run_command(cmd) # run the command as a subprocess
+
+    def run_command(self, command):
+        with subprocess.Popen(command, stdout=subprocess.PIPE) as proc:
+            print(proc.stdout.read().decode('utf-8'))
+
+"""
+driver scripting stuff ...
+"""
 root = tk.Tk()
-tutorial_driver = Tutorial(root) # does this need to be an instance object?
+tutorial_driver = Tutorial(root, title="big boy window", prompt="say hello")
 
 # open a terminal for printing UI input's output?
-terminal_win = subprocess.Popen(['x-terminal-emulator'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+# terminal_win = subprocess.Popen(['x-terminal-emulator'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+# # TODO: pass input from Tutorial Frame to terminal proc's STDIN
+# # kun here, you can also do 
+# # name = input('what is your name?: ')
 
 root.mainloop()
+
+"""
+extra stuff
+"""
+# ip = '192.168.1.1'
+
+# root = tk.Tk()
+# tfield = tk.Text(root)
+# tfield.pack()
+# f = os.popen('ping %s' % (ip))
+# for line in f:
+#     line = line.strip()
+#     if line:
+#         tfield.insert("end", line+"\n")
+#         tfield.get("current linestart", "current lineend")
+
+# def get_info(arg):
+#     print(tfield.get("1.0", "current lineend"))
+
+# tfield.bind("<Return>", get_info)
+
+# root.mainloop()
+# f.close()
+
+"""
+opens an embedded terminal
+"""
+# root = tk.Tk()
+# termf = tk.Frame(root, height=400, width=500)
+
+# termf.pack(fill=tk.BOTH, expand=tk.YES)
+# wid = termf.winfo_id()
+# os.system('xterm -into %d -geometry 40x20 -sb &' % wid)
+
+# root.mainloop()
+
+# root = tk.Tk()
+# tfield = tk.Text(root)
+# tfield.pack()
+# for line in os.popen("run_command", 'r'):
+# 	tfield.insert("end", line)
+
+# root.mainloop()
