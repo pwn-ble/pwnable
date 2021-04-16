02ca5262b10fb0f8696db058db43b87f4d90927c
averywald <averywald.dev@gmail.com>
2021-04-16 16:10:11 -0500

    fake terminal works

--- src/modules/cli/tutorial.py
+++ src/modules/cli/tutorial.py
@@ -22,10 +22,14 @@ class Tutorial(tk.Frame):
         self.prompt = prompt
         
         # add UI elements
-        self.cli_output = tk.Text(self.master, bg="black", fg="green").pack()
-        self.prompt_label = tk.Label(self.master, text=self.prompt).pack()
-        self.command_input = tk.Entry(self.master, textvariable=self.command_var).pack()
-        self.submit_button = tk.Button(self.master, command=self.handle_input).pack()
+        self.cli_output = tk.Text(self.master, bg="black", fg="green")
+        self.cli_output.pack() # separate pack statement to prevent variable value being cleared
+        self.prompt_label = tk.Label(self.master, text=self.prompt)
+        self.prompt_label.pack()
+        self.command_input = tk.Entry(self.master, textvariable=self.command_var)
+        self.command_input.pack()
+        self.submit_button = tk.Button(self.master, command=self.handle_input)
+        self.submit_button.pack()
 
     # TODO: def clean_input(input: str):
         
@@ -49,10 +53,18 @@ class Tutorial(tk.Frame):
         
         f = os.popen(cmd)
         for line in f:
-            # TODO: strip extra shit
-            self.cli_output.delete('1.0', 'end')
-            self.cli_output.insert(tk.END, line)
-            self.cli_output.update()
+            cli_val = self.cli_output.get("1.0", tk.END)
+            if (cli_val): # if the fake terminal already has content
+                # TODO: strip extra shit
+
+                # self.cli_output.delete('1.0', 'end')
+                self.cli_output.insert(tk.END, "\n" + line) # append to end of content
+                
+            else:
+                # add
+                self.cli_output.insert("1.0", line) # add to the beginning of content
+
+        self.cli_output.update()
 
 """
 driver scripting stuff ...
