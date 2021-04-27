fd47b841165f5ff3ef91ec18df6f27ec07c01f5e
averywald <averywald.dev@gmail.com>
2021-04-27 12:54:39 -0500

    popups can wait for each other

--- src/modules/cli/Tutorial.py
+++ src/modules/cli/Tutorial.py
@@ -20,10 +20,14 @@ class Tutorial(tk.Frame):
     def run(self):
         """
         TODO: include tutorial scripting stuff in order here
+        TODO: wrap popup/wait cycle in an abstraction?
         """
         greeting_popup = self.show_popup("welcome to the cli tutorial") # greet the user
-        
+        greeting_popup.wait_window() # wait for the window to be closed before continuing
+
+        ls_p = self.show_popup("the first thing you'll want to do is find out where you are.\nyou can do this by typing 'ls'.")
 
     def show_popup(self, text: str):
         popup = Popup(self.master, text = text) # greet the user
         popup.lift(self.terminal) # show up before the terminal
+        return popup
