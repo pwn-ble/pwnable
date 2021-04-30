c8a30ef44e6fa12cbb281b4dea4f84a5f836a9d0
averywald <averywald.dev@gmail.com>
2021-04-29 22:08:52 -0500

    logically reorganized app.py to help

--- src/app.py
+++ src/app.py
@@ -3,20 +3,18 @@ import platform
 import subprocess
 import tkinter as tk
 # UI component class imports
-from gui.Terminal import Terminal
-from gui.Login import Login
 from gui.Menu import Menu
-from gui.PasswordGenerator import PasswordGenerator
-from modules.cli.Tutorial import Tutorial
+from gui.Login import Login
 from gui.AddUser import AddUser
+from gui.Terminal import Terminal
+# Driver class imports
+from modules.cli.Tutorial import Tutorial
+# Misc class imports
+from gui.PasswordGenerator import PasswordGenerator
 
-root = tk.Tk()
-# TODO: show menu
-# TODO: have menu make user log in or create user
+root = tk.Tk() # app-wide tkinter window instance
 menu_driver = Menu(root)
-
-# TODO: write challenge prompts
-# TODO: build popups to supplement challenge walkthroug
+# TODO: have menu make user log in or create user
 
 # login_driver = Login(root) # begin by having the user log in
 # TODO: check progress, if any
@@ -24,5 +22,7 @@ menu_driver = Menu(root)
 # login_driver.wait_window() # wait for login process to finish
 
 # tutorial_driver = Tutorial(root)
+# TODO: write challenge prompts
+# TODO: build popups to supplement challenge walkthroug
 
 root.mainloop()
